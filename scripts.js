76ecf85884af6433ec9e96684c66ba7bb5f35454
David N <bigair_1080@hotmail.com>
2019-04-29 11:13:31 -0300

    updated files

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
-let retrieveCareerSection = () => {
-let url = './enscape.json';
 
-  fetch(url)
-    .then( res => res.json())
-    .then( data => console.log(data))
-}
+let url = 'https://confidenceiskey.github.io/test10/enscape.json';
+
+fetch(url)
+  .then( res => res.json())
+  .then( data => {
+
+    let ul = document.createElement("ul");
+    document.getElementById("enscapeHistory").appendChild(ul);
+
+    for (let i = 0; i < data.enscape.length; i++) {
+      if (moment(data.enscape[i]).format('YYYY') === '2017') {
+        console.log('true');
+      }
+      let li = document.createElement("li");
+      let node = document.createTextNode(data.enscape[i].date);
+      li.appendChild(node);
+      document.querySelector('ul').appendChild(li);
+    }
+  });
